e82ea75739feb23f14b9c5e6c6fedee0f32ef143
itskhurram <itskhurrams@outlook.com>
2021-03-18 01:56:03 +0500

    mongoos error

--- configureDatabase.js
+++ configureDatabase.js
@@ -1,19 +1,24 @@
-"use strict";
-
+'use strict';
 
 module.exports = class ConfigureDatabase {
-  constructor(environment,mongoose) {
-    mongoose
-      .connect(environment.DATABASE_URI, {
+  constructor(_environment, _mongoose) {
+    this.mongoose = _mongoose;
+    this.environment = _environment;
+    this.mongoose
+      .connect(this.environment.DATABASE_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
       .then((result) => {
-        console.log("connected to MongoDB database!");
+        console.log('connected to MongoDB database!');
         //console.log(result);
+        //return this.mongoose;
       })
       .catch((err) => {
         console.log(err);
       });
   }
+  getMongoose = () => {
+    return this.mongoose;
+  };
 };
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
-const Environment= require('./environment');
-const ConfigureDatabase =  require('./configureDatabase');
-
+const Environment = require('./environment');
+const ConfigureDatabase = require('./configureDatabase');
+const UserRepository = require('./userRepository');
+const UserService = require('./userService');
 
 const app = express();
 
-const env =  new Environment(dotenv);
-const dbConfig = new ConfigureDatabase(env,mongoose);
-
-
+const env = new Environment(dotenv);
 console.log(env);
+const dbConfigObject = new ConfigureDatabase(env, mongoose);
+const dbConfig = dbConfigObject.getMongoose();
+console.log(dbConfig);
 
+const repo = new UserRepository(dbConfig);
+const service = new UserService(repo);
+service.getUsers();
+
+console.log(service.getUsers());
 
 app.listen(env.PORT, () => {
-    console.log(`Server started at `+env.URL+ env.PORT);
+  console.log(`Server started at ` + env.URL + ':' + env.PORT);
 });
